6178ec0929214092586c4f2a315c6459cb5151b3
maxdrv <maxdrv.stud@gmail.com>
2022-09-18 09:53:23 +0300

    unimplement CharSequence xd

--- src/main/java/com/home/dictionary/model/configuration/ApiPropertyKey.java
+++ src/main/java/com/home/dictionary/model/configuration/ApiPropertyKey.java
@@ -1,21 +1,7 @@
 package com.home.dictionary.model.configuration;
 
-public enum ApiPropertyKey implements CharSequence {
+public enum ApiPropertyKey {
 
-    REGISTRATION_ALLOWED;
+    REGISTRATION_ALLOWED
 
-    @Override
-    public int length() {
-        return this.name().length();
-    }
-
-    @Override
-    public char charAt(int index) {
-        return this.name().charAt(index);
-    }
-
-    @Override
-    public CharSequence subSequence(int start, int end) {
-        return this.name().subSequence(start, end);
-    }
 }
